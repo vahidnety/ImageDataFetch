095534c915be698d96e046af19c8552aa4566f85
Seyedvahid Dianat <vahidnety@gmail.com>
2020-11-02 14:16:19 +0800

    commit

--- ImageDataFetch/ViewController.swift
+++ ImageDataFetch/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
- 
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
