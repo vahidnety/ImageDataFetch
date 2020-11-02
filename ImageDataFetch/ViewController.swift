603a33a354c257a7d7ffde888fc41d781923dc7b
Seyedvahid Dianat <vahidnety@gmail.com>
2020-11-02 13:23:12 +0800

    Initial Commit

---
+++ ImageDataFetch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageDataFetch
+//
+//  Created by Vahid on 02/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
